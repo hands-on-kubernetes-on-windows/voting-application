f27953ccc4c337ea49c7d2708894671e5804cdef
Piotr Tylenda <tylenda.piotr@gmail.com>
2019-11-16 23:10:39 +0100

    Sanitization of connection string for logging

--- Factories/VotingApplicationContextFactory.cs
+++ Factories/VotingApplicationContextFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace VotingApplication.Factories
 {
     using System;
+    using System.Data.Common;
 
     using Ninject.Activation;
     using Ninject.Extensions.Logging;
@@ -23,12 +24,24 @@
             var connectionString = Environment.GetEnvironmentVariable("CONNECTIONSTRING_VotingApplication");
             if (!string.IsNullOrEmpty(connectionString))
             {
-                this.log.Info("Using custom connection string provided by environment variable: {0}", connectionString);
+                var safeConnectionString = SanitizeConnectionString(connectionString);
+                this.log.Info("Using custom connection string provided by environment variable: {0}", safeConnectionString);
                 return new VotingApplicationContext(connectionString);
             }
 
             this.log.Info("Using default connection string");
             return new VotingApplicationContext();
+        }
+
+        private static string SanitizeConnectionString(string connectionString)
+        {
+            var builder = new DbConnectionStringBuilder { ConnectionString = connectionString };
+            if (builder.ContainsKey("password"))
+            {
+                builder["password"] = "*****";
+            }
+
+            return builder.ToString();
         }
     }
 }
