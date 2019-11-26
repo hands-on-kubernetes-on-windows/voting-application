a145dbabfd2072431732a0c6317d81a4f9e246e8
Piotr Tylenda <tylenda.piotr@gmail.com>
2019-11-26 01:18:15 +0100

    Readiness probe

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -10,6 +10,11 @@
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Health",
+                url: "health",
+                defaults: new { controller = "Health", action = "CheckHealth" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
